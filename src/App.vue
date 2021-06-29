63a4dff387b57570bc09717e8fcc9db588b6317c
Felix Raue <f.raue@rooom.com>
2021-06-29 18:32:09 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Babylon" />
 </template>
 
 <script lang="ts">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,61 @@
 <template>
-  <h1>{{ msg }}</h1>
+	<h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<canvas id="reder-canvas" ref="renderCanvas" />
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, onMounted } from "vue";
+
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import "@babylonjs/core/Helpers/sceneHelpers";
+
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+	name: "HelloWorld",
+	props: {
+		msg: {
+			type: String,
+			required: true,
+		},
+	},
+	setup: () => {
+		const renderCanvas = ref();
+
+		const createScene = function () {
+			const engine = new Engine(renderCanvas.value, true);
+			const scene = new Scene(engine);
+
+			MeshBuilder.CreateBox("box", { size: 1 }, scene);
+			scene.createDefaultCameraOrLight(true, true, true);
+			(scene.activeCamera as ArcRotateCamera).beta -= 0.4;
+			scene.createDefaultEnvironment();
+
+			engine.runRenderLoop(() => scene.render());
+
+			debug(scene);
+		};
+
+		const debug = async (scene: Scene) => {
+			await import("@babylonjs/inspector");
+			await import("@babylonjs/core/Debug/debugLayer");
+			scene.debugLayer.show();
+		};
+
+		onMounted(() => {
+			createScene();
+		});
+
+		return { renderCanvas };
+	},
+});
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+#render-canvas {
+	width: 50%;
+	height: 50%;
 }
 </style>
